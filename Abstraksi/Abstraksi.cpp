9ca7e3f1d3be67b1ad40cb4c347e821451738a77
emzhran <zachran750@gmail.com>
2023-05-29 15:11:03 +0700

    class Abstraksiklas & int main

---
+++ Abstraksi/Abstraksi.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class Abstraksiklas {
+private: string x, y; 
+
+public:
+
+
+	//method untuk mengisi nilai 
+	//private member
+	void setXY(string a, string b) {
+		x = a;
+		y = b;
+	}
+	//menampilkan nilai
+	void display() {
+		cout << "x =" << x << endl;
+		cout << "y =" << y << endl;
+	}
+};
+
+int main() {
+	Abstraksiklas ak;
+	ak.setXY("Yogyakarta", "Kampus");
+	ak.display();
+
+	return 0;
+}
