88d00c26bcbf409abf2fef65140608e5f85bebb8
emzhran <zachran750@gmail.com>
2023-05-29 10:55:39 +0700

    class pekerja yang diwarisi oleh class orang

--- ParadigmaOOP_2/ParadigmaOOP_2.cpp
+++ ParadigmaOOP_2/ParadigmaOOP_2.cpp
@@ -9,3 +9,12 @@ public:
 		cout << "orang dibuat dengan umur" << umur << "\n" << endl;
 	}
 };
+
+class pekerja :public orang {
+public:
+
+	pekerja(int pUmur) :
+		orang(pUmur) {
+		cout << "pekerja dibuat\n" << endl;
+	}
+};
